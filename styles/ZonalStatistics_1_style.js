44021cdc351e8f3547c1b438492636c1ca389d46
crossbillforestry <162478607+crossbillforestry@users.noreply.github.com>
2024-03-11 15:53:15 +0000

    Update ZonalStatistics_1_style.js

--- styles/ZonalStatistics_1_style.js
+++ styles/ZonalStatistics_1_style.js
@@ -1,7 +1,7 @@
 var size = 0;
 var placement = 'point';
 
-var style_ZonalStatistics_1 = function(feature, resolution){
+var style_Plantingopportunity_1 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
@@ -22,35 +22,35 @@ var style_ZonalStatistics_1 = function(feature, resolution){
     }
     if (value >= 0.000000 && value <= 12170.800000) {
             style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.6)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(247,252,245,0.6)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.7509999999999999)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(247,252,245,0.7509999999999999)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })]
                     } else if (value >= 12170.800000 && value <= 40276.400000) {
             style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.6)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(201,234,194,0.6)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.7509999999999999)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(201,234,194,0.7509999999999999)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })]
                     } else if (value >= 40276.400000 && value <= 63082.200000) {
             style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.6)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(123,199,124,0.6)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.7509999999999999)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(123,199,124,0.7509999999999999)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })]
                     } else if (value >= 63082.200000 && value <= 78490.600000) {
             style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.6)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(42,146,75,0.6)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.7509999999999999)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(42,146,75,0.7509999999999999)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })]
                     } else if (value >= 78490.600000 && value <= 156422.000000) {
             style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.6)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(0,68,27,0.6)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.7509999999999999)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(0,68,27,0.7509999999999999)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
