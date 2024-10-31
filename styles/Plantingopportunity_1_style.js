5ff3be3476ba5da7ca0123dfe664b476bd5a23a7
Ben Crisford <ben@crossbillforestry.co.uk>
2024-10-31 15:49:57 +0000

    On branch master  Initial commit  Changes to be committed: 	new file:   images/0.png 	new file:   images/1.png 	new file:   images/10.png 	new file:   images/100.png 	new file:   images/15.png 	new file:   images/20.png 	new file:   images/25.png 	new file:   images/30.png 	new file:   images/35.png 	new file:   images/40.png 	new file:   images/45.png 	new file:   images/5.png 	new file:   images/50.png 	new file:   images/55.png 	new file:   images/60.png 	new file:   images/65.png 	new file:   images/70.png 	new file:   images/75.png 	new file:   images/80.png 	new file:   images/85.png 	new file:   images/90.png 	new file:   images/95.png 	new file:   index.html 	new file:   layers/Plantingopportunity_1.js 	new file:   layers/layers.js 	new file:   resources/Autolinker.min.js 	new file:   resources/Autolinker.min.js.map 	new file:   resources/OSMBuildings-OL3.js 	new file:   resources/filter.css 	new file:   resources/fontawesome-all.min.css 	new file:   resources/functions.js 	new file:   resources/horsey.min.css 	new file:   resources/horsey.min.js 	new file:   resources/marker.png 	new file:   resources/materialize.min.css 	new file:   resources/materialize.min.js 	new file:   resources/measure-control.png 	new file:   resources/nouislider.min.css 	new file:   resources/nouislider.min.js 	new file:   resources/ol-geocoder.js 	new file:   resources/ol-geocoder.min.css 	new file:   resources/ol-layerswitcher.css 	new file:   resources/ol-layerswitcher.js 	new file:   resources/ol.css 	new file:   resources/ol.css.map 	new file:   resources/ol.js 	new file:   resources/ol.js.map 	new file:   resources/ol3-search-layer.js 	new file:   resources/ol3-search-layer.min.css 	new file:   resources/polyfills.js 	new file:   resources/proj4.js 	new file:   resources/qgis2web.css 	new file:   resources/qgis2web.js 	new file:   resources/qgis2web_expressions.js 	new file:   resources/tailDT.css 	new file:   styles/Plantingopportunity_1_style.js 	new file:   styles/legend/Plantingopportunity_1_0.png 	new file:   styles/legend/Plantingopportunity_1_1.png 	new file:   styles/legend/Plantingopportunity_1_2.png 	new file:   styles/legend/Plantingopportunity_1_3.png 	new file:   styles/legend/Plantingopportunity_1_4.png 	new file:   webfonts/fa-solid-900.ttf 	new file:   webfonts/fa-solid-900.woff2

---
+++ styles/Plantingopportunity_1_style.js
@@ -0,0 +1,61 @@
+var size = 0;
+var placement = 'point';
+
+var style_Plantingopportunity_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("opp_score");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    if (value >= 0.000000 && value <= 64.434000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.7)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(247,252,245,0.7)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 64.434000 && value <= 365.638000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.7)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(201,234,194,0.7)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 365.638000 && value <= 619.890000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.7)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(123,199,124,0.7)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 619.890000 && value <= 787.476000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.7)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(42,146,75,0.7)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 787.476000 && value <= 1561.360000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.7)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(0,68,27,0.7)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    };
+
+    return style;
+};
